9f2c872c071b4ab3ec69b33c7299a19f5d04cb35
Domin C <dominikcedro00@gmail.com>
2023-12-18 20:48:17 +0100

    exercise_2.py corrected

--- exercise_2.py
+++ exercise_2.py
@@ -5,42 +5,54 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+
+
 def task1(hd):
-    how_many_males = hd[(hd['Sex'] == 'male')].shape[0]
-    disease_group_male = hd[(hd['Sex'] == 'male') & (hd['Disease'] == True)].shape[0]
-    # percentage_males = (disease_group_male/how_many_males)*100
-    how_many_females = hd[(hd['Sex'] == 'female')].shape[0]
-    disease_group_female = hd[(hd['Sex'] == 'female') & (hd['Disease'] == True)].shape[0]
-    # percentage_females = (disease_group_female/how_many_females)*100
-    # print(f"among males {round(percentage_males,2)}% are sick, among females {round(percentage_females,2)}% are sick")
+    """ This function creates stacked bar chart representing health status among sexes.
 
-    females_values = [(how_many_females-disease_group_female), disease_group_female]
-    females_labels = ['Healthy', 'Sick']
-    males_values = [how_many_males-disease_group_female, disease_group_male]
-    males_labels = ['Healthy', 'Sick']
-    categories = ['Men', 'Women']
-    bar_width = 0.30
-    index = np.arange(len(categories))
+    Args:
+        hd: Heart disease dataset
+    """
+    male_sick = hd[(hd['Sex'] == 'male') & (hd['Disease'] == True)].shape[0]
+    male_healthy = hd[(hd['Sex'] == 'male') & (hd['Disease'] == False)].shape[0]
+    female_sick = hd[(hd['Sex'] == 'female') & (hd['Disease'] == True)].shape[0]
+    female_healthy = hd[(hd['Sex'] == 'female') & (hd['Disease'] == False)].shape[0]
+    sick = [male_sick, female_sick]
+    healthy = [male_healthy, female_healthy]
+
+    total_males = sum(sick[:1] + healthy[:1])
+    total_females = sum(sick[1:] + healthy[1:])
+    percent_sick_m = (sick[0] / total_males) * 100
+    percent_sick_f = (sick[1] / total_females) * 100
+
+    categories = ['Males', 'Females']
     fig, ax = plt.subplots()
-    ax.bar(categories, [females_values[0], males_values[0]],color='darkgrey', label='Healthy')
-    ax.bar(categories, [females_values[1], males_values[1]],bottom=[females_values[0], males_values[0]], color= 'orange',label='Sick')
+    bars1 = ax.bar(categories, sick, color='orange', label='Sick')
+    bars2 = ax.bar(categories, healthy, bottom=sick, color='gray', label='Healthy')
     ax.set_xlabel('Gender')
-    ax.set_ylabel('Number of People')
-    ax.set_title('Distribution of Health Status by Gender')
-    # change the colors to green and grey
-
-
+    ax.set_ylabel('Number of Individuals')
+    ax.set_title('Health Status by Gender')
     ax.legend()
+    ax.text(categories[0], sick[0] / 2, f'{percent_sick_m:.2f}%', ha='center', va='center', color='white',
+            fontsize=10)
+    ax.text(categories[1], sick[1] / 2, f'{percent_sick_f:.2f}%', ha='center', va='center', color='white',
+            fontsize=10)
     plt.show()
 
+
 def task2(hd):
+    """ Generates bar graph comparing mean value of serum cholesterol in mg/dl among different health groups.
+
+    Args:
+        hd: Heart disease dataset
+    """
     males_healthy_cholesterol = hd.loc[
         (hd['Sex'] == 'male') & (hd['Disease'] == False), 'Serum cholesterol in mg/dl'].mean()
     males_ill_cholesterol = hd.loc[(hd['Sex'] == 'male') & (hd['Disease'] == True), 'Serum cholesterol in mg/dl'].mean()
     females_healthy_cholesterol = hd.loc[
         (hd['Sex'] == 'female') & (hd['Disease'] == False), 'Serum cholesterol in mg/dl'].mean()
-    females_ill_cholesterol = hd.loc[(hd['Sex'] == 'female') & (hd['Disease'] == True), 'Serum cholesterol in mg/dl'].mean()
+    females_ill_cholesterol = hd.loc[(hd['Sex'] == 'female') & (hd['Disease'] == True),
+    'Serum cholesterol in mg/dl'].mean()
     categories = ['Female disease', 'Female healthy', 'Male disease', 'Male healthy']
     fig, ax = plt.subplots()
     bar_width = 0.35
@@ -55,19 +67,28 @@ def task2(hd):
     ax.set_xlabel('Groups')
     ax.set_ylabel('Mean value of serum cholesterol in mg/dl')
     ax.set_title('Comparison of mean value of serum cholesterol in mg/dl')
-    # Display
     plt.show()
 
+
 def task3(hd):
+    """ Generates histogram of people with heart diseases.
+    Args:
+        hd: Heart disease dataset
+    """
     disease = hd[hd['Disease'] == True]
     plt.hist(disease['Age'])
     plt.title('Histogram of people with heart diseases')
+    plt.hist(disease['Age'], color='green')
     plt.xlabel('Age')
     plt.ylabel('Number of people')
     plt.show()
 
 
 def task4(hd):
+    """ Generates box plot for the maximum achieved heart rate.
+    Args:
+        hd: Heart disease dataset
+    """
     disease_heartrate = hd.loc[hd['Disease'] == True, 'Maximum heart rate achieved'].values
     healthy_heartrate = hd.loc[hd['Disease'] == False, 'Maximum heart rate achieved'].values
     plt.boxplot([disease_heartrate, healthy_heartrate], labels=['Disease', 'Healthy'])
@@ -76,11 +97,16 @@ def task4(hd):
     plt.ylabel('Maximum achieved heart rate')
     plt.show()
 
+
 def task5(hd):
+    """ Generates bar chart representing frequency of heart disease occurrence based on exercise-induced angina.
+    Args:
+        hd: Heart disease dataset
+    """
     freq_matrix = pd.crosstab(index=hd['Disease'], columns=hd['Exercise induced angina'])
-    freq_matrix.plot(kind='bar', color=['blue', 'red'])
+    freq_matrix.plot(kind='bar', color=['gray', 'orange'])
     plt.title('Frequency of Heart Disease Occurrence based on Exercise-Induced Angina')
-    plt.xlabel('Disease')
+    plt.xlabel('Disease present')
     plt.ylabel('Frequency')
     plt.show()
 
@@ -88,10 +114,11 @@ def task5(hd):
 def main():
     hd = pd.read_csv("heart_disease_dataset.csv")
     task1(hd)
-    # task2(hd)
-    # task3(hd)
-    # task4(hd)
-    # task5(hd)
+    task2(hd)
+    task3(hd)
+    task4(hd)
+    task5(hd)
+
 
 if __name__ == "__main__":
     main()
