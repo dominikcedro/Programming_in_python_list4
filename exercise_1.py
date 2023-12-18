06cf0cf56de3516535dce36e7d644cf24c4ccd2c
Domin C <dominikcedro00@gmail.com>
2023-12-18 19:48:07 +0100

    exercise_1.py finally prepared, now focused on exercise_2.py and pdf file

--- exercise_1.py
+++ exercise_1.py
@@ -9,41 +9,50 @@ from scipy.stats import ttest_1samp
 
 
 def generate_height_data(size=1000, mean=170, st_dev=10):
-    """
-    Generates a dataset of heights.
-    :param size: size of the dataset
-    :param mean: mean of the dataset
-    :param st_dev: standard deviation of the dataset
-    :return: dataset of heights
+    """ Generates a dataset of heights.
+    Args:
+        size: int, size of the dataset
+        mean: int, mean of the dataset
+        st_dev: int, standard deviation of the dataset
+    Returns:
+         dataset of heights
     """
     return np.random.normal(mean, st_dev, size)
 
+
 def descriptive_statistics(height_data):
-    """
-    Calculates descriptive statistics of the dataset.
-    :param height_data: dataset of heights
-    :return: mean, median, standard deviation, variance, min, max
+    """ Calculates descriptive statistics of the dataset.
+    Args:
+        height_data: dataset of heights
+    Returns:
+         printed (mean, median, standard deviation, variance, min, max)
     """
     print(f"mean: {np.mean(height_data)}")
     print(f"median: {np.median(height_data)}")
     print(f"standard deviation: {np.std(height_data)}")
 
-def visualise_histogram(height_data):
-    """
-    Visualises the dataset as a histogram.
-    :param height_data: dataset of heights
-    """
 
+def visualise_histogram(height_data):
+    """Visualises the dataset as a histogram.
+    Args:
+         height_data: dataset of heights
+    """
     plt.hist(height_data)
+    plt.title("Histogram of heights")
+    plt.xlabel("Height")
+    plt.ylabel("Frequency")
+    plt.hist(height_data, color="green")
     plt.show()
 
 
-
 def calculate_percentiles(height_data):
-    """
-    Calculates percentiles of the dataset.
-    :param height_data: dataset of heights
-    :return: 25th, 50th, 75th percentiles
+    """ Calculates percentiles of the dataset.
+    Args:
+        height_data: dataset of heights
+    Returns:
+         p25: float, 25th percentile
+         p50: float, 50th percentile
+         p75: float, 75th percentile
     """
     p25 = np.percentile(height_data, 25)
     p50 = np.percentile(height_data, 50)
@@ -52,11 +61,13 @@ def calculate_percentiles(height_data):
     print(f"percentiles are: {p25}, {p50}, {p75}")
     return p25, p50, p75
 
+
 def identify_outliers(height_data):
-    """
-    Identifies outliers in the dataset.
-    :param height_data: dataset of heights
-    :return: outliers
+    """ Identifies outliers in the dataset.
+    Args:
+        height_data: dataset of heights
+    Returns:
+         outliers: list of outliers
     """
     percentiles = calculate_percentiles(height_data)
     iqr = percentiles[2] - percentiles[0]
@@ -65,44 +76,41 @@ def identify_outliers(height_data):
     print(f"outliers are: {height_data[(height_data < lower) | (height_data > upper)]}")
     return height_data[(height_data < lower) | (height_data > upper)]
 
+
 def random_sampling(height_data):
     """
     Performs random sampling on the dataset.
-    :param height_data: dataset of heights
-    :return: random sample
+    Args:
+        height_data: dataset of heights
+    Returns:
+        random sample: np array of 50 random samples
     """
     return np.random.choice(height_data, 50)
+
 
 def hypothesis_testing(data, null_hypothesis_mean=165):
     """
     Performs hypothesis testing on the dataset.
-    :param height_data: dataset of heights
-    :return: p-value
+    Args:
+        data: dataset of heights
+        null_hypothesis_mean: int, mean of the null hypothesis
+    Returns:
+        p-value: float, p-value of the test
     """
+    test = ttest_1samp(data, null_hypothesis_mean)
+    print(f"p-value: {test[1]}")
 
-    return ttest_1samp(data, null_hypothesis_mean)
 
 def calculate_probability(data, threshold_height=180):
     """
     Calculates the probability of a person being taller than a threshold height.
-    :param height_data: dataset of heights
-    :return: probability
+    Args:
+        data: dataset of heights
+        threshold_height: int, threshold height
+    Returns:
+        probability: np float, probability of a person being taller than a threshold height
     """
-    return np.sum(data > threshold_height) / len(data)
+    result = np.sum(data > threshold_height) / len(data)
+    print(f"probability: {result}")
 
-def main():
-    """
-    Main function. to change it late
-    """
-    height_data = generate_height_data()
-    descriptive_statistics(height_data)
-    visualise_histogram(height_data)
-    calculate_percentiles(height_data)
-    identify_outliers(height_data)
-    random_sample = random_sampling(height_data)
-    # print(f"random sample: {random_sample}")
-    print(f"p-value: {hypothesis_testing(height_data)}")
-    print(f"probability: {calculate_probability(height_data)}")
 
-if __name__ == "__main__":
-    main()
--- exercise_2.py
+++ exercise_2.py
@@ -5,45 +5,53 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas import DataFrame
+import numpy as np
 def task1(hd):
-    # Answer the question of whether, according to the analysed dataset, more women or
-    # men suffer from heart diseases. By what percentage?
-    how_many_males =  hd[(hd['Sex'] == 'male')].shape[0]
+    how_many_males = hd[(hd['Sex'] == 'male')].shape[0]
     disease_group_male = hd[(hd['Sex'] == 'male') & (hd['Disease'] == True)].shape[0]
-    percentage_males = (disease_group_male/how_many_males)*100
+    # percentage_males = (disease_group_male/how_many_males)*100
     how_many_females = hd[(hd['Sex'] == 'female')].shape[0]
     disease_group_female = hd[(hd['Sex'] == 'female') & (hd['Disease'] == True)].shape[0]
-    percentage_females = (disease_group_female/how_many_females)*100
-    print(f"among males {round(percentage_males,2)}% are sick, among females {round(percentage_females,2)}% are sick")
+    # percentage_females = (disease_group_female/how_many_females)*100
+    # print(f"among males {round(percentage_males,2)}% are sick, among females {round(percentage_females,2)}% are sick")
 
+    females_values = [(how_many_females-disease_group_female), disease_group_female]
+    females_labels = ['Healthy', 'Sick']
+    males_values = [how_many_males-disease_group_female, disease_group_male]
+    males_labels = ['Healthy', 'Sick']
+    categories = ['Men', 'Women']
+    bar_width = 0.30
+    index = np.arange(len(categories))
+    fig, ax = plt.subplots()
+    ax.bar(categories, [females_values[0], males_values[0]],color='darkgrey', label='Healthy')
+    ax.bar(categories, [females_values[1], males_values[1]],bottom=[females_values[0], males_values[0]], color= 'orange',label='Sick')
+    ax.set_xlabel('Gender')
+    ax.set_ylabel('Number of People')
+    ax.set_title('Distribution of Health Status by Gender')
+    # change the colors to green and grey
+
+
+    ax.legend()
+    plt.show()
 
 def task2(hd):
-    # 2. Compare the average value of serum cholesterol separately for the group of women
-    # and the group of men depending on the presence of heart disease.
-    general_view_cholesterol = hd.groupby(['Sex', 'Disease'])['Serum cholesterol in mg/dl'].mean()
-    # males
     males_healthy_cholesterol = hd.loc[
         (hd['Sex'] == 'male') & (hd['Disease'] == False), 'Serum cholesterol in mg/dl'].mean()
     males_ill_cholesterol = hd.loc[(hd['Sex'] == 'male') & (hd['Disease'] == True), 'Serum cholesterol in mg/dl'].mean()
-    # females
     females_healthy_cholesterol = hd.loc[
         (hd['Sex'] == 'female') & (hd['Disease'] == False), 'Serum cholesterol in mg/dl'].mean()
     females_ill_cholesterol = hd.loc[(hd['Sex'] == 'female') & (hd['Disease'] == True), 'Serum cholesterol in mg/dl'].mean()
     categories = ['Female disease', 'Female healthy', 'Male disease', 'Male healthy']
-    # plot
     fig, ax = plt.subplots()
     bar_width = 0.35
     color1 = 'skyblue'
     color2 = 'dodgerblue'
     color3 = 'lightgreen'
     color4 = 'forestgreen'
-    # plot bars
     bar1 = ax.bar(categories[0], females_ill_cholesterol, bar_width, color=color1, label='Bar 1')
     bar2 = ax.bar(categories[1], females_healthy_cholesterol, bar_width, color=color2, label='Bar 2')
     bar3 = ax.bar(categories[2], males_ill_cholesterol, bar_width, color=color3, label='Bar 2')
     bar4 = ax.bar(categories[3], males_healthy_cholesterol, bar_width, color=color4, label='Bar 2')
-    # Add labels
     ax.set_xlabel('Groups')
     ax.set_ylabel('Mean value of serum cholesterol in mg/dl')
     ax.set_title('Comparison of mean value of serum cholesterol in mg/dl')
@@ -51,8 +59,6 @@ def task2(hd):
     plt.show()
 
 def task3(hd):
-    # 3. Draw a histogram of people with heart diseases. In which age range are the most
-    # affected individuals?
     disease = hd[hd['Disease'] == True]
     plt.hist(disease['Age'])
     plt.title('Histogram of people with heart diseases')
@@ -62,54 +68,30 @@ def task3(hd):
 
 
 def task4(hd):
-    # 4. Draw a box plot for the maximum achieved heart rate during the exercise test
-    # depending on the presence of heart disease. What observations can be made based
-    # on this plot?
     disease_heartrate = hd.loc[hd['Disease'] == True, 'Maximum heart rate achieved'].values
     healthy_heartrate = hd.loc[hd['Disease'] == False, 'Maximum heart rate achieved'].values
-
     plt.boxplot([disease_heartrate, healthy_heartrate], labels=['Disease', 'Healthy'])
     plt.title('Box plot for the maximum achieved heart rate')
     plt.xlabel('Groups')
     plt.ylabel('Maximum achieved heart rate')
-    plt.legend()
     plt.show()
 
 def task5(hd):
-    # 5. Draw a bar chart for the frequency of heart disease occurrence depending on whether
-    # the patient has angina during the exercise test. What observations can be made
-    # based on the chart?
-    #
-    # angina = hd.loc[hd['Exercise induced angina'] == True, 'Disease'].count()
-    # no_angina = hd.loc[hd['Exercise induced angina'] == False, 'Disease'].count()
-    # print(angina, no_angina)
-    #
-    # plt.bar(['Angina', 'No angina'], [angina, no_angina])
-    # plt.title('Frequency of heart disease occurrence')
-    # plt.xlabel('Groups')
-    # plt.ylabel('Frequency')
-    # plt.show()
-    frequency_table = pd.crosstab(index=hd['Disease'], columns=hd['Exercise induced angina'])
-
-    # Plot the bar chart
-    frequency_table.plot(kind='bar', stacked=True, color=['blue', 'orange'])
-
-    # Add labels and title
+    freq_matrix = pd.crosstab(index=hd['Disease'], columns=hd['Exercise induced angina'])
+    freq_matrix.plot(kind='bar', color=['blue', 'red'])
     plt.title('Frequency of Heart Disease Occurrence based on Exercise-Induced Angina')
     plt.xlabel('Disease')
     plt.ylabel('Frequency')
-
-    # Show the plot
     plt.show()
 
 
 def main():
     hd = pd.read_csv("heart_disease_dataset.csv")
-    # task1(hd)
+    task1(hd)
     # task2(hd)
     # task3(hd)
     # task4(hd)
-    task5(hd)
+    # task5(hd)
 
 if __name__ == "__main__":
     main()
